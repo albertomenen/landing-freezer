64d9712324a9dea094bedf0dbdcfb89b1f63c79d
Alberto <alberto.menendez89@gmail.com>
2024-10-02 15:37:15 +0200

    stable version comeback

---
+++ static/chunks/pages/blog-9606fb657ef734e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[195],{5870:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog",function(){return _(461)}])},461:function(n,u,_){"use strict";_.r(u);var e=_(1527);_(959);let t=()=>(0,e.jsx)("div",{children:"Blog Page"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5870)}),_N_E=n.O()}]);
